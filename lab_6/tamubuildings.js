ca7e58bc8497829ec65ee35127c3dd0d14914bee
llong07 <llong.07@tamu.edu>
2025-05-01 08:54:51 -0600

    Add files via upload

---
+++ lab_6/tamubuildings.js
@@ -0,0 +1,62 @@
+var tamuBuildings = {
+    "type": "FeatureCollection",
+    "features": [
+    {
+        "type": "Feature",
+        "properties": {
+        "BLDG_NAME": "Evans Library",
+        "BLDG_ABBR": "LIBR"
+        },
+        "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-96.3395, 30.6177],
+                [-96.3392, 30.6177],
+                [-96.3392, 30.6174],
+                [-96.3395, 30.6174],
+                [-96.3395, 30.6177]
+            ]
+        ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+        "BLDG_NAME": "Academic Building",
+        "BLDG_ABBR": "ACAD"
+        },
+        "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-96.3401, 30.6182],
+                [-96.3398, 30.6182],
+                [-96.3398, 30.6179],
+                [-96.3401, 30.6179],
+                [-96.3401, 30.6182]
+            ]
+        ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+        "BLDG_NAME": "Rudder Tower",
+        "BLDG_ABBR": "RUDR"
+        },
+        "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-96.3375, 30.6180],
+                [-96.3373, 30.6180],
+                [-96.3373, 30.6178],
+                [-96.3375, 30.6178],
+                [-96.3375, 30.6180]
+            ]
+        ]
+        }
+    }
+    ]
+};
